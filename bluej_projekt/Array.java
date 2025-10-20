b5cbe61e86d44bc0f186ddb154a2c81bb18209f9
bie240592 <bie240592@plf.spengergasse.at>
2025-10-20 14:56:10 +0200

    autocommit

--- bluej_projekt/Array.java
+++ bluej_projekt/Array.java
@@ -11,18 +11,22 @@ public class Array {
     // übergebenen int Array
     public static int arraySum(int[] arr) {
         int rv = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            rv = arr[i];
+        }
         return rv;
     }
 
     // hier wird das größte Element des Arrays gesucht
     public static int arrayMax(int[] arr) {
-        int rv = Integer.MIN_VALUE;
+        int rv = Integer.MAX_VALUE;
         return rv;
     }
 
     // hier wird das kleinste Element des Arrays gesucht
     public static int arrayMin(int[] arr) {
-        int rv = Integer.MAX_VALUE;
+        int rv = Integer.MIN_VALUE;
         return rv;
     }
 
